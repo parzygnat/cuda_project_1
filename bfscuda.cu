136f58b81fc9e69c2fe3438ba55a0a9be927a696
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-10 05:39:30 +0100

    now?

--- bfscuda.cu
+++ bfscuda.cu
@@ -268,10 +268,11 @@ void runGpu(int startVertex, Graph &G) {
         cudaDeviceSynchronize();
         for(int i = 0; i < num_vertices; i++) printf("%d ", distances[i]); 
         level++;
-        break;
     }
     
     printf("\n the size of the new queue is %d", *v_queuesize);
+    for(int i = 0; i < v_queuesize; i++) printf("%d ", v_queuesize[i]); 
+
     v_queuesize = 0;
     auto end = std::chrono::system_clock::now();
     float duration = 1000.0*std::chrono::duration<float>(end - start).count();
