15f2a0e2dbb28b50dcbdc6e4c48c49fb8bfa5960
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-10 17:20:00 +0100

    ccc

--- bfscuda.cu
+++ bfscuda.cu
@@ -240,10 +240,10 @@ __global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_que
     //now we compact
     if(b1_initial[local_tid])
     {
-        printf("im thread %d and my number is %d and my index is %d\n", tid, e_queue[local_tid], temp + b2_initial[local_tid]);
         int temp = block_alloc_size[tid>>10];
         if (gridDim.x == 1) temp = 0;
         distances[e_queue[local_tid]] = level + 1;
+        printf("im thread %d and my number is %d and my index is %d\n", tid, e_queue[local_tid], temp + b2_initial[local_tid]);
         v_queue[temp + b2_initial[local_tid]] = e_queue[local_tid];
     }
     }
