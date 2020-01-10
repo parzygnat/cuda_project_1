39687ed117ccb41afa9e830d875d3c5950e5a9b0
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-10 04:13:15 +0100

    see you Radek

--- bfscuda.cu
+++ bfscuda.cu
@@ -250,6 +250,7 @@ void runGpu(int startVertex, Graph &G) {
         mem = mem*2*sizeof(int);
         printf("the new queue size is %d\n", *e_queuesize);
         contraction<<<num_blocks, 1024, mem>>>(cvector, rvector, v_queue, e_queue, v_queuesize, e_queuesize, block_alloc_size, distances, level);
+        cudaDeviceSynchronize();
         level++;
         break;
     }
