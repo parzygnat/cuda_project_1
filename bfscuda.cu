1f6848b2f294088ab52709aaaf71750e08810522
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-10 02:25:11 +0100

    split into 2 kernels for expanion and contraction

--- bfscuda.cu
+++ bfscuda.cu
@@ -3,7 +3,7 @@
 #include "graph.h"
 #include "bfscpu.h"
 #include <queue>
-#include <thrust/copy.h>
+#include <thrust/reduce.h>
 
 
 void bfsCPU(Graph &G) {
@@ -38,20 +38,21 @@ void runCpu(int startVertex, Graph &G) {
     
 }
 
-__global__ void cudabfs(int* cvector, int* rvector, int* c_queue, int* n_queue, int c_queuesize, int n_queuesize, int* block_alloc_size, int* distances, int level)
+__global__ void expansion(int* cvector, int* rvector, int* c_queue, int* n_queue, int c_queuesize, int* n_queuesize, int* block_alloc_size, int* distances, int level)
 {
-    int tid = threadIdx.x;
+    int tid = blockIdx.x *blockDim.x + threadIdx.x;
     int _initial;
-
+    int local_tid = threadIdx.x;
+    
     if(tid < c_queuesize) {
         __shared__ int prefixSum[1024];
-        int local_tid = threadIdx.x;
         int degree = 0;
         int u = c_queue[tid];
+        //we create a block shared array of degrees of the elements of the current vertex frontier
         prefixSum[tid] = rvector[u + 1] - rvector[u];
-        __shared__ int int_initial[1024];
-        __shared__ int b_initial[1024];
-                
+        
+        //1s of 3 scans in this algorithm - we calculate offsets for writing ALL neighbors into a block shared array
+        // blelloch exclusive scan algorithm with upsweep to the left
         for (int nodeSize = 2; nodeSize <= 1024; nodeSize <<= 1) {
             __syncthreads();
             if ((local_tid & (nodeSize - 1)) == 0) {
@@ -61,14 +62,17 @@ __global__ void cudabfs(int* cvector, int* rvector, int* c_queue, int* n_queue, 
                 }
             }
         }
+
         if (local_tid == 0) {
             int block = tid >> 10;
-            initial = block_alloc_size[block + 1] = prefixSum[local_tid];
+            // the efect of upsweep - reduction of the whole array (number of ALL neighbors)
+            e_queuesize = block_alloc_size[block + 1] = prefixSum[local_tid];
         }
+        //downsweep - now our array prefixSum has become a prefix sum of numbers of neighbors
         for (int nodeSize = 1024; nodeSize > 1; nodeSize >>= 1) {
             __syncthreads();
             if ((local_tid & (nodeSize - 1)) == 0) {
-                if (thid + (nodeSize >> 1) < c_queuesize) {
+                if (tid + (nodeSize >> 1) < c_queuesize) {
                     int next_position = local_tid + (nodeSize >> 1);
                     int tmp = prefixSum[local_tid];
                     prefixSum[local_tid] -= prefixSum[next_position];
@@ -77,48 +81,115 @@ __global__ void cudabfs(int* cvector, int* rvector, int* c_queue, int* n_queue, 
                 }
             }
         }
+        //scan on offsets produced by blocks in total
+        if(gridDim.x > 1) {
+            if(tid < gridDim.x) {
+                for (int nodeSize = 2; nodeSize <= gridDim.x; nodeSize <<= 1) {
+                    __syncthreads();
+                    if ((tid & (nodeSize - 1)) == 0) {
+                        if (tid + (nodeSize >> 1) < gridDim.x) {
+                            int nextPosition = tid + (nodeSize >> 1);
+                            block_alloc_size[tid] += block_alloc_size[nextPosition];
+                        }
+                    }
+                }
+                if (tid == 0) {
+                    *n_queuesize = block_alloc_size[tid];
+                }
+                for (int nodeSize = 1024; nodeSize > 1; nodeSize >>= 1) {
+                    __syncthreads();
+                    if ((tid & (nodeSize - 1)) == 0) {
+                        if (tid + (nodeSize >> 1) < c_queuesize) {
+                            int next_position = tid + (nodeSize >> 1);
+                            int tmp = block_alloc_size[tid];
+                            block_alloc_size[tid] -= block_alloc_size[next_position];
+                            block_alloc_size[next_position] = tmp;
+                        }
+                    }
+                }
+            }
+        }
+
         int iter = 0;
         for(int i = rvector[u]; i < rvector[u + 1]; i++) {
-            int_initial[iter + prefixSum[tid]] = cvector[i];
+            e_queue[iter + prefixSum[tid] + block_alloc_size[tid>>10]] = cvector[i];
             iter++;
         }
     }
-    
-    if(tid < _initial) {
-        b_initial[tid] = true;
-        if(distances[int_initial[tid]] < 0)
-            b_initial[tid] = false;
+}
+__global__ void contraction(int* cvector, int* rvector, int* c_queue, int* n_queue, int c_queuesize, int* n_queuesize, int* block_alloc_size, int* distances, int level)
+{
+    int tid = blockIdx.x *blockDim.x + threadIdx.x;
+    int _initial;
+    int local_tid = threadIdx.x;
 
-            
+    if(tid < e_queuesize) {
+        b_initial[local_tid] = 1;
+        if(distances[int_initial[local_tid]] < 0)
+            b1_initial[local_tid] = 0;
+        b2_initial[local_tid] = b1_initial[local_tid]
+
+
         for (int nodeSize = 2; nodeSize <= 1024; nodeSize <<= 1) {
             __syncthreads();
             if ((local_tid & (nodeSize - 1)) == 0) {
                 if (tid + (nodeSize >> 1) < _initial) {
                     int nextPosition = local_tid + (nodeSize >> 1);
-                    prefixSum[local_tid] += prefixSum[nextPosition];
+                    b2_initial[local_tid] += b2_initial[nextPosition];
                 }
             }
         }
         if (local_tid == 0) {
             int block = tid >> 10;
-            initial = block_alloc_size[block + 1] = prefixSum[local_tid];
+            _initial = block_alloc_size[block] = prefixSum[local_tid];
         }
         for (int nodeSize = 1024; nodeSize > 1; nodeSize >>= 1) {
             __syncthreads();
             if ((local_tid & (nodeSize - 1)) == 0) {
-                if (thid + (nodeSize >> 1) < _initial) {
+                if (tid + (nodeSize >> 1) < _initial) {
                     int next_position = local_tid + (nodeSize >> 1);
-                    int tmp = prefixSum[local_tid];
-                    prefixSum[local_tid] -= prefixSum[next_position];
-                    prefixSum[next_position] = tmp;
-
+                    int tmp = b2_initial[local_tid];
+                    b2_initial[local_tid] -= b2_initial[next_position];
+                    b2_initial[next_position] = tmp;
+                }
+            }
+        }
+        __syncthreads();
+    }
+    if(gridDim.x > 1) {
+        if(tid < gridDim.x) {
+            for (int nodeSize = 2; nodeSize <= gridDim.x; nodeSize <<= 1) {
+                __syncthreads();
+                if ((tid & (nodeSize - 1)) == 0) {
+                    if (tid + (nodeSize >> 1) < gridDim.x) {
+                        int nextPosition = tid + (nodeSize >> 1);
+                        block_alloc_size[tid] += block_alloc_size[nextPosition];
+                    }
+                }
+            }
+            if (tid == 0) {
+                *n_queuesize = block_alloc_size[tid];
+            }
+            for (int nodeSize = 1024; nodeSize > 1; nodeSize >>= 1) {
+                __syncthreads();
+                if ((tid & (nodeSize - 1)) == 0) {
+                    if (tid + (nodeSize >> 1) < c_queuesize) {
+                        int next_position = tid + (nodeSize >> 1);
+                        int tmp = block_alloc_size[tid];
+                        block_alloc_size[tid] -= block_alloc_size[next_position];
+                        block_alloc_size[next_position] = tmp;
+                    }
                 }
             }
         }
     }
-        
+    
+    if(tid < *n_queuesize)
+    {
+        n_queue[block_alloc_size[tid>>10] + local_tid] = int1_initial[local_tid];
     }
-}
+    }
+
 
 void runGpu(int startVertex, Graph &G) {
     G.root = startVertex;
@@ -126,17 +197,17 @@ void runGpu(int startVertex, Graph &G) {
     printf("Starting cuda  bfs.\n\n\n");
     int level = 0;
     int num_blocks;
-    int* c_queue;
-    int* n_queue;
+    int* v_queue;
+    int* e_queue;
     int* block_alloc_size;
     int* distances;
     int* cvector;
     int* rvector;
-    int c_queuesize;
+    int e_queuesize;
     int n_queuesize;
     int num_vertices = G.rvector.size() - 1;
-    cudaMallocManaged(&c_queue, num_vertices*sizeof(int));
-    cudaMallocManaged(&n_queue, num_vertices*sizeof(int));
+    cudaMallocManaged(&v_queue, num_vertices*sizeof(int));
+    cudaMallocManaged(&e_queue, num_vertices*sizeof(int));
     cudaMallocManaged(&block_alloc_size, num_vertices*sizeof(int)/1024 + 1);
     cudaMallocManaged(&distances, num_vertices*sizeof(int));
     cudaMallocManaged(&cvector, G.cvector.size()*sizeof(int));
@@ -144,17 +215,21 @@ void runGpu(int startVertex, Graph &G) {
     std::copy(G.cvector.begin(), G.cvector.end(), cvector);
     std::copy(G.rvector.begin(), G.rvector.end(), rvector);
     c_queue[0] = G.root;
+    block_alloc_size[0] = 0;
     c_queuesize = 1;
     level = 0;
     n_queuesize = 0;
     auto start = std::chrono::system_clock::now();
     printf("im working\n");
     num_blocks = c_queuesize/1024 + 1;
-    cudabfs<<<num_blocks, 1024>>>(cvector, rvector, c_queue, n_queue, c_queuesize, n_queuesize, block_alloc_size, distances, level);
-    printf("it is indeed %d", c_queue[0]);
+    expansion<<<num_blocks, 1024>>>(cvector, rvector, v_queue, e_queue, c_queuesize, &e_queuesize, block_alloc_size, distances, level);
+    contraction<<<num_blocks, 1024>>>(cvector, rvector, v_queue, e_queue, c_queuesize, &e_queuesize, block_alloc_size, distances, level);
+
+    printf("the size of the new queue is %d", n_queuesize);
     c_queuesize = 0;
     auto end = std::chrono::system_clock::now();
     float duration = 1000.0*std::chrono::duration<float>(end - start).count();
+    for(int i )
     printf("\n \n\nElapsed time in milliseconds : %f ms.\n\n", duration);
     cudaFree(c_queue);
     cudaFree(n_queue);
