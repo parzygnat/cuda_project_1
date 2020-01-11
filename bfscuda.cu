a71d0672619bd8637bcd05e90cb2fd9dbf14699c
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-11 23:31:09 +0100

    n

--- bfscuda.cu
+++ bfscuda.cu
@@ -212,6 +212,7 @@ __global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_que
         if (local_tid == 0) {
             int block = tid >> 10;
             // the efect of upsweep - reduction of the whole array (number of ALL neighbors)
+            printf("n is %d\n", n);
             *v_queuesize = b1_initial[n - 1];
             printf("i work6");
             return;
