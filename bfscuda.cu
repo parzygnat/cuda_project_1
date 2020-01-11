65822852fe06f79fcf8d4f4d83a8dfd8c853163a
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-11 22:37:20 +0100

    asd

--- bfscuda.cu
+++ bfscuda.cu
@@ -193,11 +193,11 @@ __global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_que
             b1_initial[local_tid] = 0;
     }
 
-    printf("i work4");
 
     if(tid < extra) {
     // we create a copy of this and make an array with scan of the booleans. this way we will know how many valid neighbors are there to check
         b2_initial[local_tid] = b1_initial[local_tid];
+        printf("i work4");
 
         offset = 1;
         for (int d = n>>1; d > 0; d >>=1) {
