5cb5b122f8ee0bd89fec4a3c7eaa78235433bc36
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-12 02:29:13 +0100

    now

--- bfscuda.cu
+++ bfscuda.cu
@@ -47,7 +47,7 @@ void runCpu(int startVertex, Graph &G) {
     
 }
 
-__global__ void expansion(int* cvector, int* rvector, int* v_queue, int* e_queue, volatile int *v_queuesize, volatile int* e_queuesize, int* v_block_alloc_size, int* e_block_alloc_size, int* distances, int level, int extra)
+__global__ void expansion(int* cvector, int* rvector, int* v_queue, int* e_queue, int *v_queuesize, int* e_queuesize, volatile int* v_block_alloc_size, volatile int* e_block_alloc_size, int* distances, int level, int extra)
 {
     int tid = blockIdx.x *blockDim.x + threadIdx.x;
     int local_tid = threadIdx.x;
@@ -164,7 +164,7 @@ __global__ void expansion(int* cvector, int* rvector, int* v_queue, int* e_queue
 }
 }
 
-__global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_queue, volatile int *v_queuesize, volatile int* e_queuesize, int* v_block_alloc_size, int* e_block_alloc_size, int* distances, int level, int extra)
+__global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_queue, int *v_queuesize,  int* e_queuesize, volatile int* v_block_alloc_size, volatile int* e_block_alloc_size, int* distances, int level, int extra)
 {
     int tid = blockIdx.x *blockDim.x + threadIdx.x;
     int local_tid = threadIdx.x;
