5393217bb0456de98e3ddab9a73b8a586e74777c
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-12 01:17:58 +0100

    kinda check ehh

--- bfscuda.cu
+++ bfscuda.cu
@@ -289,7 +289,7 @@ __global__ void contraction(int* cvector, int* rvector, int* v_queue, int* e_que
         return;
     }
     //now we compact
-    if(tid < *e_queuesize && (b1_initial[local_tid] != b1_initial[local_tid + 1]))
+    if(tid < *e_queuesize)
     {
         int ver = e_queue[tid];
         int temp = v_block_alloc_size[tid>>10];
