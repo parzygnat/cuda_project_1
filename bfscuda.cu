ebce97a22e3717ecf64426c72de993141e30d9a2
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-13 00:43:42 +0100

    debugging

--- bfscuda.cu
+++ bfscuda.cu
@@ -111,7 +111,7 @@ __global__ void expansion(int* cvector, int* rvector, int* v_queue, int* e_queue
         }
 
     __syncthreads();
-
+    if(local_tid == 0) printf("dzialam %d \n", blockIdx.x);
     if(local_tid == 0) {
         block_alloc_size = atomicAdd(counter, total);
     }
