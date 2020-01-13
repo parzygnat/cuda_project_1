6cfe35beebf0e0382ea04e6685c0eeae7326fa56
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-13 19:50:46 +0100

    c segfault

--- main.cpp
+++ main.cpp
@@ -4,24 +4,57 @@
 #include "graph.h"
 #include "bfsCPU.h"
 
-void runCpu(int startVertex, Graph &G) {
-    G.root = startVertex;
-    printf("Starting sequential bfs.\n");
-    auto start = std::chrono::steady_clock::now();
-    bfsCPU(startVertex, G);
-    auto end = std::chrono::steady_clock::now();
-    long duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    for(int i = 0; i < G.distances.size(); i++) printf("%d ", G.distances[i]);
-    printf("\n Elapsed time in milliseconds : %li ms.\n\n", duration);
+void bfsCPU(Graph &G) {
+    G.distances[G.root] = 0;
+    std::queue<int> Q;
+    Q.push(G.root);
 
+    while (!Q.empty()) {
+        int u = Q.front();
+        Q.pop();
+
+        for (int i = G.rvector[u]; i < G.rvector[u + 1]; i++) {
+            int v = G.cvector[i];
+            if (G.distances[v] == -1) {
+                G.distances[v] = G.distances[u] + 1;
+                Q.push(v);
+            }
+        }
+    }
 }
 
-int main(int argc, char **argv) {
-    // read graph from standard input
+void runCpu(int startVertex, Graph &G) {
+    G.root = startVertex;
+    for (int i = 0; i < G.rvector.size() - 1; i++) G.distances.push_back(-1);
+    printf("Starting sequential bfs.\n\n\n");
+    auto start = std::chrono::system_clock::now();
+    return;
+    bfsCPU(G);
+    auto end = std::chrono::system_clock::now();
+    float duration = 1000.0*std::chrono::duration<float>(end - start).count();
+    //for (int i = G.rvector.size() - 10; i < G.rvector.size() - 1; i++) printf(" %d ", G.distances[i]);
+    printf("\n \n\nElapsed time in milliseconds : %f ms.\n\n", duration);
+    
+}
+
+
+int main(void)
+{
     Graph G;
-    G.cvector = {1, 3, 0, 2, 4, 4, 5, 7, 8, 6, 8};
-    G.rvector = {0, 2, 5, 5, 6, 8, 9, 9, 11, 11};
+    for(int i = 1; i < 1 + 100 + 1000 + 10000; i++){
+        G.cvector.push_back(i);
+    }
+    for(int i = 0; i < 1 + 100 + 1000 + 10000 + 1; i++) {
+        if(i == 0)
+        G.rvector.push_back(0);
+        else if(i < 1 + 100 + 1000)
+        G.rvector.push_back(100*i);
+        else
+        G.rvector.push_back(100 + 1000 + 10000);
+    }    
+
     //run CPU sequential bfs
     runCpu(0, G);
+
     return 0;
 }
