751633cc18f05d19b3c6855d2eaacad781510bff
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-13 01:00:49 +0100

    sync experiment

--- bfscuda.cu
+++ bfscuda.cu
@@ -110,10 +110,13 @@ __global__ void expansion(int* cvector, int* rvector, int* v_queue, int* e_queue
         }
     }
 
+    __syncthreads();
 
     if(local_tid == 0) {
         block_alloc_size = atomicAdd(counter, total);
     }
+
+    __syncthreads();
 
 
     if(tid < *v_queuesize) {
