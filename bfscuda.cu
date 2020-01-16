81a96e62fde01383562da022114ff0567b44f983
Paweł Grabowski <p.grabowski2@student.mini.pw.edu.pl>
2020-01-16 20:18:40 +0100

    printing

--- bfscuda.cu
+++ bfscuda.cu
@@ -241,7 +241,7 @@ void runGpu(int startVertex, Graph &G) {
     auto end = std::chrono::system_clock::now();
     float duration = 1000.0*std::chrono::duration<float>(end - start).count();
     printf("\n \n\nElapsed time in milliseconds : %f ms.\n\n", duration);
-    //for (int i = 0; i < 150; i++) printf(" %d ", distances[i]);
+    for (int i = 0; i < 8; i++) printf(" %d ", distances[i]);
     //for (int i = G.rvector.size() - 10; i < G.rvector.size() - 1; i++) printf(" %d ", distances[i]);
     cudaFree(v_queuesize);
     cudaFree(e_queuesize);
